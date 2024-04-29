db8aac63d8261eb97405c0e36a95dad2976eb132
trupletj <tuvshinjargal01@gmail.com>
2024-04-29 20:08:53 +0800

    fixed

--- auth.config.ts
+++ auth.config.ts
@@ -7,23 +7,23 @@ import Google from "next-auth/providers/google";
 
 export default {
   providers: [
-    Credentials({
-      async authorize(credentials) {
-        const validatedFields = LoginSchema.safeParse(credentials);
-        if (validatedFields.success) {
-          const { email, password } = validatedFields.data;
+    // Credentials({
+    //   async authorize(credentials) {
+    //     const validatedFields = LoginSchema.safeParse(credentials);
+    //     if (validatedFields.success) {
+    //       const { email, password } = validatedFields.data;
 
-          const user = await getUserByEmail(email);
-          if (!user || !user.password) return null;
+    //       const user = await getUserByEmail(email);
+    //       if (!user || !user.password) return null;
 
-          const passwordMatch = await comparePassword(password, user.password);
-          if (passwordMatch) {
-            return user;
-          }
-        }
-        return null;
-      },
-    }),
+    //       const passwordMatch = await comparePassword(password, user.password);
+    //       if (passwordMatch) {
+    //         return user;
+    //       }
+    //     }
+    //     return null;
+    //   },
+    // }),
     Google,
   ],
 };
